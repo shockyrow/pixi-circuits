c3b8428c26998d57afc06dbbf53f9d97f8e9b72a
Ibrohim Shokirov <ishokirov@hotmail.com>
2026-04-30 12:51:23 +0700

    Implemented CircuitApp.

--- src/app.ts
+++ src/app.ts
@@ -2,3 +2,19 @@ export interface App {
   run(): void;
   destroy(): void;
 }
+
+export class CircuitApp implements App {
+  private container: HTMLElement;
+
+  constructor(container: HTMLElement) {
+    this.container = container;
+  }
+
+  run(): void {
+    console.log("Running...");
+  }
+
+  destroy(): void {
+    console.log("Destroying...");
+  }
+}
--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,11 @@
-console.log('Yes');
+import { CircuitApp } from "./app";
+
+const container = document.getElementById("app");
+
+if (container === null) {
+  throw new Error("Container not found");
+}
+
+const app = new CircuitApp(container);
+app.run();
+app.destroy();
